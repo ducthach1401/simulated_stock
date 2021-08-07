de12625da69ac06157d7628dae44196ff86d524a
Thach <ducthach.1401@gmail.com>
2021-08-07 20:21:03 +0700

    update

--- src/service/stock.js
+++ src/service/stock.js
@@ -5,12 +5,13 @@ module.exports.getStocks = async () =>{
     request({
         method: 'GET',
         url: 'https://banggia.cafef.vn/stockhandler.ashx'
-    }, (err, res, body) => {
+    }, async (err, res, body) => {
         if (err) return console.error(err);
         const data = JSON.parse(body);
         let list = {};
-        for (let stock of data){
+        for await (let stock of data){
             list[stock.a] = stock.b;
         }
+        return list;
     });
-}    
+}
