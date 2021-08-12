c1c598f6e665c0270c5e04c8954ac53283aa40b6
Thach <ducthach.1401@gmail.com>
2021-08-12 14:42:18 +0700

    update

--- src/view/js/home.js
+++ src/view/js/home.js
@@ -1,5 +1,5 @@
-// const API_URL = "https://simulatestock.glitch.me/";
-const API_URL = "http://localhost:8080";
+const API_URL = "https://simulatestock.glitch.me/";
+// const API_URL = "http://localhost:8080";
 
 var dataStockGobal = profit();
 
