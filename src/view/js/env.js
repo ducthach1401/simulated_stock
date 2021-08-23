0526ff4a34cd185bf63dbcc141c35944bdd891ff
Thach <ducthach.1401@gmail.com>
2021-08-23 22:22:16 +0700

    update

--- src/view/js/env.js
+++ src/view/js/env.js
@@ -1,6 +1,6 @@
 // const API_URL = "https://simulatestock.glitch.me";
-// const API_URL = "http://localhost:8080";
-const API_URL = "https://simulatestock.herokuapp.com";
+// const API_URL = "https://simulatestock.herokuapp.com";
+const API_URL = "http://localhost:8080";
 
 async function refreshToken(){
     const url = API_URL + '/user/v1/refresh';
