97b47d4134dcf47823e5c1c245128702ead83897
Luca <59497044+ducthach1401@users.noreply.github.com>
2021-09-23 09:12:34 +0700

    Update env.js

--- src/view/js/env.js
+++ src/view/js/env.js
@@ -1,6 +1,6 @@
 // const API_URL = "https://simulatestock.glitch.me";
-// const API_URL = "https://simulatestock.herokuapp.com";
-const API_URL = "http://localhost:8080";
+const API_URL = "https://simulatestock.herokuapp.com";
+//const API_URL = "http://localhost:8080";
 
 async function logout(){
     window.location.href = '/logout';
